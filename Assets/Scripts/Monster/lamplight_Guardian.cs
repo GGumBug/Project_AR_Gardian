5e4fd268ee458cddc93c4b82992bfbe1cd7174a8
GGumBug <alsrl7538@gmail.com>
2023-01-18 14:48:37 +0900

    등불도깨비 스크립트 추가

---
+++ Assets/Scripts/Monster/lamplight_Guardian.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lamplight_Guardian : GuardianBase
+{
+    private void Start()
+    {
+        
+    }
+}
