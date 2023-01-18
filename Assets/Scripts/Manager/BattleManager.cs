4ba709f2d88675cf72503cbd480fa94105806182
GGumBug <alsrl7538@gmail.com>
2023-01-18 16:19:33 +0900

    불필요 코드 삭제

--- Assets/Scripts/Manager/BattleManager.cs
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -23,27 +23,4 @@ public class BattleManager : MonoBehaviour
     #endregion
     public int curGuardian;//몬스터 순번 0이면 1단계 1이면 2단계 이런식
 
-    void Update()
-    {
-
-    }
-    public void BattleStart()
-    {
-
-    }
-    public void GuardianAppear()
-    {
-        ObjectManager.GetInstance().CreateGuardian(curGuardian);
-    }
-
-    void TouchGuardian() // 나중에머 도깨비불에 터치하면 모델링 생성되겠금하는 함수 아직안씀 ㅇㅇ
-    {
-        if (Input.GetTouch(0).phase == TouchPhase.Ended)
-        {
-            {
-                GuardianAppear();
-            }
-        }
-
-    }
 }
