7d55cd5937d63d0497ec1effc82112e008e430b0
GGumBug <alsrl7538@gmail.com>
2023-01-18 10:24:11 +0900

    Wisp 자동 생성 코드 추가

--- Assets/Scripts/Manager/ObjectManager.cs
+++ Assets/Scripts/Manager/ObjectManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ObjectManager : MonoBehaviour
 {
@@ -21,9 +22,43 @@ public class ObjectManager : MonoBehaviour
 
     }
     #endregion
+
+    ARRaycastManager arRaycast;
+
+    GameObject monsterPref;
+
+    private void Awake()
+    {
+        GameObject arSessionOrigin = GameObject.FindGameObjectWithTag("ARSessionOrigin");
+        arRaycast = arSessionOrigin.GetComponent<ARRaycastManager>();
+    }
+
+    private void Update()
+    {
+        var screenPoint = Camera.current.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        if (arRaycast.Raycast(screenPoint, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes))
+        {
+            Pose planePos = hits[0].pose;
+
+            monsterPref = CreateWisp();
+
+            monsterPref.transform.position = planePos.position;
+            monsterPref.transform.rotation = planePos.rotation;
+
+            monsterPref.SetActive(true);
+        }
+        else
+        {
+            monsterPref.SetActive(false);
+        }
+    }
+
     public GameObject CreateWisp()
     {
-        Object characterWisp = Resources.Load($"Object/Wisp");
+        Object characterWisp = Resources.Load($"Object/Wisp_{BattleManager.GetInstance().curGuardian}");
         GameObject wisp = (GameObject)Instantiate(characterWisp);
         return wisp;
     }
@@ -33,8 +68,4 @@ public class ObjectManager : MonoBehaviour
         GameObject Guardian = (GameObject)Instantiate(GuardianObj);
         return Guardian;
     }
-
-
-
-
 }
