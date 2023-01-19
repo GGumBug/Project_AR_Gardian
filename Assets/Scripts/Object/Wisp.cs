fbda2be5fd39824123358feeee476ffe4c365e67
GGumBug <alsrl7538@gmail.com>
2023-01-19 11:40:31 +0900

    위습 찾으면 더 이상 생성 안되게 수정

--- Assets/Scripts/Object/Wisp.cs
+++ Assets/Scripts/Object/Wisp.cs
@@ -8,6 +8,7 @@ public class Wisp : MonoBehaviour
     public void OnMouseDown()
     {
         ObjectManager.GetInstance().CreateGuardian(BattleManager.GetInstance().curGuardian);
+        SpawnManager.GetInstance().page_1 = true;
         Destroy(gameObject);
     }
 }
