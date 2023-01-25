f080e676b62139e163d41b9ae4a1fdcf3aa65157
GGumBug <alsrl7538@gmail.com>
2023-01-25 22:10:21 +0900

    Debug용 스크립트 추가

---
+++ Assets/Scripts/DebugTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugTest : MonoBehaviour
+{
+    [SerializeField]Button btnWisp;
+    [SerializeField]GameObject wisp;
+
+    public void DebugCreatWisp()
+    {
+        wisp = ObjectManager.GetInstance().CreateWisp();
+        wisp.transform.position = SpawnManager.GetInstance().spawnPosition.position;
+    }
+}
