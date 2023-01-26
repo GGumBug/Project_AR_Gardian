745cfaef1c7819e32e95c422d5cdb41e7de8517c
GGumBug <alsrl7538@gmail.com>
2023-01-26 14:24:40 +0900

    패링 4방향 구현

--- Assets/Scripts/Guardian/GuardianBase.cs
+++ Assets/Scripts/Guardian/GuardianBase.cs
@@ -8,10 +8,12 @@ public class GuardianBase
     public int atk { get; protected set; }
     public int maxHp { get; set; }
     public int hp { get; set; }
+    public int attackDirection;
     public float delay { get; protected set; }
     public bool isClear { get; set; }
     public bool canAttack { get; set; }
     public bool canParrying { get; set; }
+
 
     public virtual GuardianBase Clone()
     {
@@ -29,6 +31,39 @@ public class GuardianBase
 
     public virtual void Attack()
     {
+        int ran = Random.Range(0, 4);
+        attackDirection = ran;
+
+        switch (attackDirection)
+        {
+            case 0:
+                // 내려찍기
+                ConfirmAttack();
+                break;
+            case 1:
+                //우공격
+                ConfirmAttack();
+                break;
+            case 2:
+                //올려치기
+                ConfirmAttack();
+                break;
+            case 3:
+                //좌공격
+                ConfirmAttack();
+                break;
+        }
+    }
+
+    void ConfirmAttack()
+    {
+        int p = GameManager.GetInstance().parryingDrection;
+
+        if (GameManager.GetInstance().NewPlayer.isParrying && attackDirection == p)
+        {
+            BattleManager.GetInstance().GuardianStun();
+            return;
+        }
         GameManager.GetInstance().SetHp(-atk);
     }
 }
