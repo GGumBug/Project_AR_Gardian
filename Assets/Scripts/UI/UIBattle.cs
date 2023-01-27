0111388094c2720ec95e474f98091b5eb4a215d5
GGumBug <alsrl7538@gmail.com>
2023-01-27 12:22:59 +0900

    SwipeSystem 생성

--- Assets/Scripts/UI/UIBattle.cs
+++ Assets/Scripts/UI/UIBattle.cs
@@ -15,14 +15,28 @@ public class UIBattle : MonoBehaviour
     [SerializeField] Slider hpPlayer;
     [SerializeField] Slider hpGuardian;
     [SerializeField] TMP_Text battleInfo;
+    [SerializeField] SwipeManager swipeManager = null;
+    [SerializeField] TestSwipeManager testSwipeManager = null;
 
     private void Start()
     {
-        btnAttack.onClick.AddListener(() => { BattleManager.GetInstance().PlayerAttack(); });
+        //btnAttack.onClick.AddListener(() => { BattleManager.GetInstance().PlayerAttack(); });
         btnTop.onClick.AddListener(() => { GameManager.GetInstance().Parrying(0); });
         btnRight.onClick.AddListener(() => { GameManager.GetInstance().Parrying(1); });
         btnBottom.onClick.AddListener(() => { GameManager.GetInstance().Parrying(2); });
         btnLeft.onClick.AddListener(() => { GameManager.GetInstance().Parrying(3); });
+
+        if (swipeManager == null)
+            swipeManager = GetComponent<SwipeManager>();
+
+        if (swipeManager == null)
+            swipeManager = gameObject.AddComponent<SwipeManager>();
+
+        if (testSwipeManager == null)
+            testSwipeManager = GetComponent<TestSwipeManager>();
+
+        if (testSwipeManager == null)
+            testSwipeManager = gameObject.AddComponent<TestSwipeManager>();
     }
 
     public void RefreshHP()
