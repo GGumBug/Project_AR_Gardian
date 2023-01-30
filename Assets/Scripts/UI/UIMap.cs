4808a7f52dd385969c1183a1baac08dfcf8cf6fa
hann <yoohan0314@naver.com>
2023-01-30 12:35:23 +0900

    UI 수정

--- Assets/Scripts/UI/UIMap.cs
+++ Assets/Scripts/UI/UIMap.cs
@@ -26,7 +26,7 @@ public class UIMap : MonoBehaviour
         {
             if (GuardianManager.GetInstance().IsOpenGardian(i) == false)
             {
-                monsterBut[i].image.sprite = Resources.Load<Sprite>("Image/DanGer");
+                monsterBut[i].image.sprite = Resources.Load<Sprite>("Image/GetGuardianBtnImg");
                 monsterBut[i].image.color = new Color(1, 1, 1, 1);
                 BattleManager.GetInstance().curGuardian = i;
                 monsterBut[i].onClick.AddListener(ChangeBattleScene);
@@ -34,28 +34,28 @@ public class UIMap : MonoBehaviour
             }
             if (GuardianManager.GetInstance().IsOpenGardian(i) == true)
             {
-                monsterBut[i].image.sprite = Resources.Load<Sprite>("Image/StageClear");
+                monsterBut[i].image.sprite = Resources.Load<Sprite>("Image/ClearFire");
             }
 
         }
     }
-        void Monsterbuthide()
-        {
+    void Monsterbuthide()
+    {
             for (int i = 0; i < monsterBut.Length; i++)
             {
                 monsterBut[i].gameObject.SetActive(false);
             }
-        }
-        void ChangeBattleScene()
-        {
+    }
+    void ChangeBattleScene()
+    {
             ScenesManager.GetInstance().ChangeScene(Scene.Battle);
 
-        }
+    }
 
         void OpenGameInfo()
-    {
-        gameInfocanvas.gameObject.SetActive(true);
-    }
+        {
+            gameInfocanvas.gameObject.SetActive(true);
+        }
 
 }
 
