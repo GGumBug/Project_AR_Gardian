eb6d06e34c8d62c8e3dc3cc2997c314e9d4d05e0
GGumBug <alsrl7538@gmail.com>
2023-02-02 12:15:21 +0900

    등불도깨비 버그 수정

--- Assets/Scripts/Scene/BattleScene.cs
+++ Assets/Scripts/Scene/BattleScene.cs
@@ -7,6 +7,7 @@ public class BattleScene : MonoBehaviour
     void Start()
     {
         GuardianManager.GetInstance().GuardianList[BattleManager.GetInstance().curGuardian].canAttack = false;
+        BattleManager.GetInstance().page = Page.page_0;
         SpawnManager.GetInstance();
         BattleManager.GetInstance();
         AudioManager.instance.bgAudioSource.loop = true;
