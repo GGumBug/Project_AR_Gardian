bff90cb96ef3a7ea1c8e192f6e8cedc34fb1cf17
GGumBug <alsrl7538@gmail.com>
2023-02-02 18:17:37 +0900

    불필요한 코드 삭제

--- Assets/Scripts/Animator/CheckParry.cs
+++ Assets/Scripts/Animator/CheckParry.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class CheckParry : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         int a = BattleManager.GetInstance().curGuardian;
@@ -24,16 +11,4 @@ public class CheckParry : StateMachineBehaviour
         BattleManager.GetInstance().page = Page.page_1;
         GuardianManager.GetInstance().GuardianList[a].canAttack = false;
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
