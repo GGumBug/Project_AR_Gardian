402b4bdb1fa938b8f9f3af18ce0b95df82ed9a85
jinjin <maroon1566@gmail.com>
2023-02-02 12:26:04 +0900

    충돌해결

--- Assets/Scripts/DebugTest.cs
+++ Assets/Scripts/DebugTest.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class DebugTest : MonoBehaviour
 {
     [SerializeField]Button btnWisp;
+    [SerializeField] Button btnsk1;
+    [SerializeField] Button btnsk2;
+
     [SerializeField]GameObject wisp;
 
     public void DebugCreatWisp()
@@ -13,4 +16,13 @@ public class DebugTest : MonoBehaviour
         wisp = ObjectManager.GetInstance().CreateWisp();
         wisp.transform.position = SpawnManager.GetInstance().spawnPosition.position;
     }
+    public void Debugski1()
+    {
+        GameManager.GetInstance().NewPlayer.skill_1 = true;
+    }
+    public void Debugsk2()
+    {
+        GameManager.GetInstance().NewPlayer.skill_2 = true;
+    }
+
 }
